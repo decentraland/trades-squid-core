9bc0bd1bfe82cb2a85d74381f7abc53a2d0fece0
Juanma Hidalgo <juanma06@gmail.com>
2025-10-26 09:38:09 -0300

    feat: track marketplace offchain v2

--- src/polygon/main.ts
+++ src/polygon/main.ts
@@ -2,15 +2,18 @@ import * as polygonMarketplaceAbi from '../abi/DecentralandMarketplacePolygon'
 import { getDb } from '../common/db'
 import { getDataHandler } from '../common/handler'
 import { createOffchainMarketplaceProcessor } from '../common/processor'
-import { ProcessorConfig, processorConfig } from '../common/utils/config'
+import { ProcessorConfig, processorConfigV1, processorConfigV2 } from '../common/utils/config'
 import { Network } from '../model'
 
-const config: ProcessorConfig = processorConfig[Network.POLYGON][process.env.POLYGON_CHAIN_ID]
+const config: ProcessorConfig = processorConfigV1[Network.POLYGON][process.env.POLYGON_CHAIN_ID]
+const configV2: ProcessorConfig = processorConfigV2[Network.POLYGON][process.env.POLYGON_CHAIN_ID]
 
 const processor = createOffchainMarketplaceProcessor({
   network: Network.POLYGON,
   address: config.marketplaceAddress,
+  addressV2: configV2.marketplaceAddress,
   fromBlock: config.fromBlock,
+  fromBlockV2: configV2.fromBlock,
   gateway: `https://v2.archive.subsquid.io/network/${config.gatewayNetwork}`,
   rpcEndpoint: process.env.RPC_ENDPOINT_POLYGON,
   abi: polygonMarketplaceAbi,

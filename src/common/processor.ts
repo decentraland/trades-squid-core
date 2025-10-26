e9b99ff39353d587ac12dbad27a2137b39152613
Juanma Hidalgo <juanma06@gmail.com>
2025-10-26 09:29:10 -0300

    feat: track marketplace offchain v2

--- src/common/processor.ts
+++ src/common/processor.ts
@@ -5,6 +5,8 @@ import { OffchainMarketplaceAbi } from './types'
 type TradesProcessorOptions = {
   network: Network
   address: string
+  addressV2: string
+  fromBlockV2: number
   gateway: string
   rpcEndpoint: string
   fromBlock: number
@@ -15,6 +17,8 @@ type TradesProcessorOptions = {
 export function createOffchainMarketplaceProcessor({
   network,
   address,
+  addressV2,
+  fromBlockV2,
   gateway,
   rpcEndpoint,
   fromBlock,
@@ -39,6 +43,20 @@ export function createOffchainMarketplaceProcessor({
     .setFinalityConfirmation(FINALITY_CONFIRMATION)
     .addLog({
       address: [address],
+      topic0: [
+        abi.events.Traded.topic,
+        abi.events.SignatureCancelled.topic,
+        abi.events.ContractSignatureIndexIncreased.topic,
+        abi.events.SignerSignatureIndexIncreased.topic,
+        abi.events.Paused.topic,
+        abi.events.Unpaused.topic
+      ]
+    })
+    .addLog({
+      address: [addressV2],
+      range: {
+        from: fromBlockV2
+      },
       topic0: [
         abi.events.Traded.topic,
         abi.events.SignatureCancelled.topic,

6379009e84b32f12a26dfe8c5bb9553812cf64cc
Juanma Hidalgo <juanma06@gmail.com>
2025-10-26 09:27:01 -0300

    feat: track marketplace offchain v2

--- src/ethereum/main.ts
+++ src/ethereum/main.ts
@@ -2,15 +2,17 @@ import * as ethereumMarketplaceAbi from '../abi/DecentralandMarketplaceEthereum'
 import { getDb } from '../common/db'
 import { getDataHandler } from '../common/handler'
 import { createOffchainMarketplaceProcessor } from '../common/processor'
-import { ProcessorConfig, processorConfig } from '../common/utils/config'
+import { ProcessorConfig, processorConfigV1, processorConfigV2 } from '../common/utils/config'
 import { Network } from '../model'
-
-const config: ProcessorConfig = processorConfig[Network.ETHEREUM][process.env.ETHEREUM_CHAIN_ID]
+const config: ProcessorConfig = processorConfigV1[Network.ETHEREUM][process.env.ETHEREUM_CHAIN_ID]
+const configV2: ProcessorConfig = processorConfigV2[Network.ETHEREUM][process.env.ETHEREUM_CHAIN_ID]
 
 const processor = createOffchainMarketplaceProcessor({
   network: Network.ETHEREUM,
   address: config.marketplaceAddress,
+  addressV2: configV2.marketplaceAddress,
   fromBlock: config.fromBlock,
+  fromBlockV2: configV2.fromBlock,
   gateway: `https://v2.archive.subsquid.io/network/${config.gatewayNetwork}`,
   rpcEndpoint: process.env.RPC_ENDPOINT_ETH,
   abi: ethereumMarketplaceAbi,

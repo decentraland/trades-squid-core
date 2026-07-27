af61bbfd0c48130b8c72079fb38c3e369eee7db1
Juanma Hidalgo <juanma06@gmail.com>
2026-07-27 11:47:22 +0200

    feat: migrate to sqd portal data source (#39)

--- src/ethereum/main.ts
+++ src/ethereum/main.ts
@@ -1,22 +1,32 @@
+import { run } from '@subsquid/batch-processor'
+import * as evmObjects from '@subsquid/evm-objects'
 import * as ethereumMarketplaceAbi from '../abi/DecentralandMarketplaceEthereum'
 import { getDb } from '../common/db'
 import { getDataHandler } from '../common/handler'
 import { createOffchainMarketplaceProcessor } from '../common/processor'
 import { ProcessorConfig, processorConfigV1, processorConfigV2 } from '../common/utils/config'
 import { Network } from '../model'
+
 const config: ProcessorConfig = processorConfigV1[Network.ETHEREUM][process.env.ETHEREUM_CHAIN_ID]
 const configV2: ProcessorConfig = processorConfigV2[Network.ETHEREUM][process.env.ETHEREUM_CHAIN_ID]
 
-const processor = createOffchainMarketplaceProcessor({
-  network: Network.ETHEREUM,
+const { dataSource, prometheus } = createOffchainMarketplaceProcessor({
   address: config.marketplaceAddress,
   addressV2: configV2.marketplaceAddress,
   fromBlock: config.fromBlock,
   fromBlockV2: configV2.fromBlock,
-  gateway: `https://v2.archive.subsquid.io/network/${config.gatewayNetwork}`,
-  rpcEndpoint: process.env.RPC_ENDPOINT_ETH,
+  portalDataset: config.gatewayNetwork,
   abi: ethereumMarketplaceAbi,
   prometheusPort: parseInt(process.env.ETH_PROMETHEUS_PORT) || 3000
 })
 
-processor.run(getDb(Network.ETHEREUM), getDataHandler(ethereumMarketplaceAbi, config.marketplaceAddress, Network.ETHEREUM))
+const handler = getDataHandler(ethereumMarketplaceAbi, config.marketplaceAddress, Network.ETHEREUM)
+
+run(
+  dataSource,
+  getDb(Network.ETHEREUM),
+  // run() hands over a bare {store, blocks, isHead}; augment the blocks so the handler
+  // sees the block.logs / log.transaction back-references it relies on.
+  ctx => handler({ ...ctx, blocks: ctx.blocks.map(evmObjects.augmentBlock) }),
+  { prometheus }
+)
--- src/polygon/main.ts
+++ src/polygon/main.ts
@@ -1,3 +1,5 @@
+import { run } from '@subsquid/batch-processor'
+import * as evmObjects from '@subsquid/evm-objects'
 import * as polygonMarketplaceAbi from '../abi/DecentralandMarketplacePolygon'
 import { getDb } from '../common/db'
 import { getDataHandler } from '../common/handler'
@@ -8,16 +10,23 @@ import { Network } from '../model'
 const config: ProcessorConfig = processorConfigV1[Network.POLYGON][process.env.POLYGON_CHAIN_ID]
 const configV2: ProcessorConfig = processorConfigV2[Network.POLYGON][process.env.POLYGON_CHAIN_ID]
 
-const processor = createOffchainMarketplaceProcessor({
-  network: Network.POLYGON,
+const { dataSource, prometheus } = createOffchainMarketplaceProcessor({
   address: config.marketplaceAddress,
   addressV2: configV2.marketplaceAddress,
   fromBlock: config.fromBlock,
   fromBlockV2: configV2.fromBlock,
-  gateway: `https://v2.archive.subsquid.io/network/${config.gatewayNetwork}`,
-  rpcEndpoint: process.env.RPC_ENDPOINT_POLYGON,
+  portalDataset: config.gatewayNetwork,
   abi: polygonMarketplaceAbi,
   prometheusPort: parseInt(process.env.POLYGON_PROMETHEUS_PORT) || 3001
 })
 
-processor.run(getDb(Network.POLYGON), getDataHandler(polygonMarketplaceAbi, config.marketplaceAddress, Network.POLYGON))
+const handler = getDataHandler(polygonMarketplaceAbi, config.marketplaceAddress, Network.POLYGON)
+
+run(
+  dataSource,
+  getDb(Network.POLYGON),
+  // run() hands over a bare {store, blocks, isHead}; augment the blocks so the handler
+  // sees the block.logs / log.transaction back-references it relies on.
+  ctx => handler({ ...ctx, blocks: ctx.blocks.map(evmObjects.augmentBlock) }),
+  { prometheus }
+)
